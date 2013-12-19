aaa22e990d2ae40559b2359f80403f98cfbb11be
Roman Cheplyaka <roma@ro-che.info>
2013-12-19 20:12:14 +0200

    Add -a option

--- ls.hs
+++ ls.hs
@@ -1,9 +1,16 @@
 import System.Directory
+import System.Environment
 
 main :: IO ()
 main =
   getDirectoryContents "." >>= \files ->
-    mapM_ putStrLn (filterDots files)
+  getArgs >>= \args ->
+    let
+      filterFn =
+        case args of
+          [] -> filterDots
+          ["-a"] -> id
+    in mapM_ putStrLn (filterFn files)
 
 filterDots :: [FilePath] -> [FilePath]
 filterDots paths = filter (\path -> head path /= '.') paths
