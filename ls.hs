8ead1c2d0a19b690515463dcae2e961788435c44
Roman Cheplyaka <roma@ro-che.info>
2013-12-19 20:05:52 +0200

    Filter dot files

--- ls.hs
+++ ls.hs
@@ -3,4 +3,7 @@ import System.Directory
 main :: IO ()
 main =
   getDirectoryContents "." >>= \files ->
-    mapM_ putStrLn files
+    mapM_ putStrLn (filterDots files)
+
+filterDots :: [FilePath] -> [FilePath]
+filterDots paths = filter (\path -> head path /= '.') paths
