449e5ce86dec8cc59d2eba843e8c4efd9a0e45a6
Roman Cheplyaka <roma@ro-che.info>
2013-12-19 20:37:05 +0200

    Use optparse-applicative

--- ls.hs
+++ ls.hs
@@ -1,16 +1,33 @@
 import System.Directory
 import System.Environment
+import Options.Applicative
+
+data OptionsT = Options
+  { ignoreDots :: Bool
+  }
 
 main :: IO ()
 main =
   getDirectoryContents "." >>= \files ->
-  getArgs >>= \args ->
+  getOurArgs >>= \opts ->
     let
       filterFn =
-        case args of
-          [] -> filterDots
-          ["-a"] -> id
+        if ignoreDots opts
+          then filterDots
+          else id
     in mapM_ putStrLn (filterFn files)
 
 filterDots :: [FilePath] -> [FilePath]
 filterDots paths = filter (\path -> head path /= '.') paths
+
+getOurArgs :: IO OptionsT
+getOurArgs = execParser $ info (helper <*> parser) idm
+  where
+    parser =
+      Options <$>
+        (not <$>
+          switch
+            (  long "all"
+            <> short 'a'
+            <> help "do not ignore entries starting with ."
+            ))
